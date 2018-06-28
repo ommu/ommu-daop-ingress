6e77c04498bf671c34c10575a66f4eb3d43eb52b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-06-28 14:10:57 +0700

    add count column condition in getInfo function

--- models/DaopAnothers.php
+++ models/DaopAnothers.php
@@ -373,13 +373,16 @@ class DaopAnothers extends CActiveRecord
 	{
 		if($column != null) {
 			$model = self::model()->findByPk($id,array(
-				'select' => $column
+				'select' => $column,
 			));
-			return $model->$column;
+ 			if(count(explode(',', $column)) == 1)
+ 				return $model->$column;
+ 			else
+ 				return $model;
 			
 		} else {
 			$model = self::model()->findByPk($id);
-			return $model;			
+			return $model;
 		}
 	}
 	
