14fb5ddbc12663bee5bb52d01697c4cda1402cf5
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 19:20:19 +0700

    commit

--- views/city/_view.php
+++ views/city/_view.php
@@ -6,7 +6,7 @@
  *
  * * @author Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
- * @copyright Copyright (c) 2014 Ommu Platform (ommu.co)
+ * @copyright Copyright (c) 2014 Ommu Platform (www.ommu.co)
  * @link https://github.com/ommu/ommu-daop-ingress
  *
  */
